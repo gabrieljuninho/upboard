4bdad75b4d5210c8dc4a3e953369321e3c8d1317
gabrieljuninho <gabrieltobing789@gmail.com>
2025-06-07 12:10:11 +0700

    chore: add axios for API requests and create api instance

---
+++ lib/api.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_APP_URL,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
